4c94c09db5e1edfe5f0ac3a336863f061dcfc75a
Rishabh Tiwari <rishabhtiwari1204@gmail.com>
2024-09-04 23:00:08 +0530

    Ep2: Igniting out app.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const heading1 = React.createElement("h1",{ id: 'head1' }, "Heading1");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1",{ id: 'head1' }, "Namaeste React");
 const heading2 = React.createElement("h2", { id: "head2" }, "Heading2");
 const container = React.createElement("div",{ id: 'container'}, [heading1, heading2]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
